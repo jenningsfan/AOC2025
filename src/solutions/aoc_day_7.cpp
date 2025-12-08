231ff47efaf3eff5cd54e2997e6b998608127328
Sammy Humphreys <Sammyh2009@outlook.com>
2025-12-08 17:03:59 +0000

    solve day 7 part 2

--- src/solutions/aoc_day_7.cpp
+++ src/solutions/aoc_day_7.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <tuple>
+#include <unordered_map>
 
 #include "aoc_day_7.h"
 #include "file_utils.h"
@@ -51,6 +52,31 @@ void update_manifold(BitGrid* splitters_map, BitGrid* splitters_hit, coord locat
     }
 }
 
+long part2_recurse(BitGrid *splitters_map, coord location, unordered_map<size_t, long> *memo) {
+    if (location.x > splitters_map->cols) {
+        return 0;
+    }
+    else if (location.y > splitters_map->rows) {
+        return 1;
+    }
+
+    size_t index = location.x + location.y * splitters_map->cols;
+    if (memo->contains(index)) {
+        return memo->at(index);
+    }
+    
+    for (size_t r = location.y; r < splitters_map->rows; r++) {
+        if (splitters_map->get(location.x, r)) {
+            long result = part2_recurse(splitters_map, coord { location.x - 1, r + 1 }, memo) +
+                part2_recurse(splitters_map, coord { location.x + 1, r + 1 }, memo);
+            (*memo)[index] = result;
+            return result;
+        }
+    }
+
+    return 1;
+}
+
 string AocDay7::part1(string filename, vector<string> extra_args)
 {
     BitGrid data = read_input(filename);
@@ -65,10 +91,9 @@ string AocDay7::part1(string filename, vector<string> extra_args)
 string AocDay7::part2(string filename, vector<string> extra_args)
 {
     BitGrid data = read_input(filename);
-    BitGrid map = BitGrid(data.rows, data.cols);
-    update_manifold(&data, &map, coord { data.cols / 2, 0 });
+    unordered_map<size_t, long> map = unordered_map<size_t, long>();
 
     ostringstream out;
-    out << map.bits_set();
+    out << part2_recurse(&data, coord { data.cols / 2, 0 }, &map);
     return out.str();
 }
